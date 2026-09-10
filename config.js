468c69b44eb0b03047dc38d68de585c69a89bf9c
Vinicius <viniciusnevez123@gmail.com>
2026-09-10 07:02:02 -0300

    Atualizar versão oficial para 1.1.0

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.APP_CONFIG=Object.freeze({env:"official",label:"Oficial",release:"1.0.1",storageKey:"simbolos.library.v2",legacyKey:"simbolos.library.v1",storageAliases:{},cachePrefix:"simbolos-",scopeNeedle:"/Simbolos/",assetRoot:"./",enableServiceWorker:false});
+window.APP_CONFIG=Object.freeze({env:"official",label:"Oficial",release:"1.1.0",storageKey:"simbolos.library.v2",legacyKey:"simbolos.library.v1",storageAliases:{},cachePrefix:"simbolos-",scopeNeedle:"/Simbolos/",assetRoot:"./",enableServiceWorker:false});
 window.APP_RELEASE=window.APP_CONFIG.release;
 window.APP_ENV=window.APP_CONFIG.env;
